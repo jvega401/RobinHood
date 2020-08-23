7fbbc0f586efd3f7c27588de81f2c0db48f1c9dd
Josue <45518920+jvega401@users.noreply.github.com>
2020-08-22 22:06:54 -0700

    modified problem description

--- meanGroups/src/Main.java
+++ meanGroups/src/Main.java
@@ -1,4 +1,6 @@
 /*
+You are given an array of arrays a. Your task is to group the arrays a[i] by their mean values, so that arrays with equal mean values are in the same group, and arrays with different mean values are in different groups.
+
 Each group should contain a set of indices (i, j, etc), such that the corresponding arrays (a[i], a[j], etc) all have the same mean. Return the set of groups as an array of arrays, where the indices within each group are sorted in ascending order, and the groups are sorted in ascending order of their minimum element.
 
 Example
@@ -62,6 +64,10 @@ Guaranteed constraints:
 1 ≤ a.length ≤ 100,
 1 ≤ a[i].length ≤ 100,
 -100 ≤ a[i][j] ≤ 100.
+
+[output] array.array.integer
+
+An array of arrays, representing the groups of indices.
  */
 
 public class Main {
